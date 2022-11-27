da542d5526ecb56175298f432b734ff771f2775e
shihyuhuang <61773397+shihyuhuang@users.noreply.github.com>
2022-11-27 19:14:28 +0800

    Add files via upload

--- main.m
+++ main.m
@@ -1,28 +1,34 @@
-n= -2;
-total_bit = 12;
-fraction = 4;
+llr=[-1.25, 3.5, 10.0, 12.75, 0, 2.125, -6.625, 7.0625];
+len=length(llr);
 
-if n > 0
-    n=n;
-    flag=0;
-else
-    n=-n;
-    flag=1;
-end
-[num,numint,numf] = dectobin1( n, fraction);
-[numo,numinto,numfo]=conv(numint, numf, flag);
-
-N=length(numo);
-sign_numo = numo(1);
-
-N= total_bit-N;
-
-for i = 1:N
-    if sign_numo == 0
-        numo = [0, numo];
+for i=1:len
+    n= llr(i);
+    total_bit = 12;
+    fraction = 4;
+    
+    if n >= 0
+        n=n;
+        flag=0;
     else
-        numo = [1, numo];
+        n=-n;
+        flag=1;
     end
+    [num,numint,numf] = dectobin1( n, fraction);
+    [numo,numinto,numfo]=conv(numint, numf, flag);
+    
+    N=length(numo);
+    sign_numo = numo(1);
+    
+    N= total_bit-N;
+    
+    for i = 1:N
+        if sign_numo == 0
+            numo = [0, numo];
+        else
+            numo = [1, numo];
+        end
+    end
+    
+    fprintf('%d', numo)
 end
 
-disp(numo)
